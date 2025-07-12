d5df1df520c818e6ec73ae50d19ffb3e85ce3527
Dropee <deepak2006ghunawat@gmail.com>
2025-07-12 21:37:05 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,1 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/Home';
-import LoginPage from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import AddProduct from './pages/AddProduct';
-
-export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/add-product" element={<AddProduct />} />
-      </Routes>
-    </Router>
-  );
-}
+import Home from './pages/Home';
